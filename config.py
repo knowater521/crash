388101c79a6bc418908eb2a02db4d0009b4622d6
zzzzer <zzzzer91@gmail.com>
2018-12-20 19:43:48 +0800

    example

---
+++ config.py
@@ -0,0 +1,16 @@
+"""配置文件"""
+
+# 使用线程数量
+THREAD_NUM = 4
+
+# MySQL 配置
+MYSQL_CONFIG = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': 'root',
+    'pwd': 'password',
+    'db': 'glasses',
+}
+
+# 保存数据的表
+MYSQL_TABLE_SAVE_EBD = 'ebd'
---
+++ main.py
@@ -0,0 +1,145 @@
+"""使用多线程库抓取 ebd 眼镜数据。
+
+create_time: 2018-12-13
+modified_time:
+"""
+
+import warnings
+import queue
+import re
+
+from lxml import etree
+
+from lib import spider, log
+from lib.types import *
+
+from config import *
+
+# warnings.filterwarnings('error')  # 将警告提升为异常，可以过滤掉不符合字段类型的数据
+
+log.logger.set_log_level(log.DEBUG)
+
+
+class EbdProductListSpider(spider.MultiThreadSpider):
+
+    url_temp = 'https://www.eyebuydirect.com/eyeglasses?' \
+               'attrs%5B%5D={}&page={}&view-column=1&pagesize=120'
+
+    shape_list = [
+        'rectangle', 'wayfarer', 'round', 'square', 'oval', 'horn', 'browline', 'aviator'
+    ]
+
+    # 提取总产品数
+    re_product_count = re.compile(r'<strong>(\d+)</strong>')
+
+    def __init__(self,
+                 name: str,
+                 mysql_config: MysqlConfig,
+                 table_save:  str,
+                 daemon: bool = True) -> None:
+        super().__init__(name, mysql_config, table_save, daemon)
+
+        self.session.headers.update(self.headers_json)
+
+    def run(self) -> None:
+        for shape in self.shape_list:
+            url_base = self.url_temp.format(shape, '{}')
+            url = url_base.format(1)
+            r = self.session.get(url)
+            product_count = int(self.re_product_count.findall(r.json()['data']['pageHtml'])[0])
+            for i in range(1, product_count // 120 + 2):
+                url = url_base.format(i)
+                r = self.session.get(url)
+                selector = etree.HTML(r.json()['data']['list'])
+                product_ele_list = selector.xpath('.//ul/li[contains(@class,"item")]')
+                for product_ele in product_ele_list:
+                    product_name = product_ele.xpath('./@data-product-name')[0]
+                    product_id = product_ele.xpath('./@data-pid')[0]
+                    product_code = product_ele.xpath('./@data-product-code')[0]
+                    product_price = product_ele.xpath('./@data-product-price')[0]
+                    product_type = product_ele.xpath('./@data-product-type')[0]
+                    product_url = 'https://www.eyebuydirect.com' + \
+                                  product_ele.xpath('.//a[@class="event-list-link"]/@href')[0]
+
+                    self.insert({
+                        'product_name': product_name,
+                        'product_id': product_id,
+                        'product_code': product_code,
+                        'url': product_url,
+                        'shape': shape,
+                        'price': product_price,
+                        'type': product_type,
+                    })
+
+
+class EbdProductDetailSpider(spider.MultiThreadSpider):
+
+    def __init__(self,
+                 name: str,
+                 mysql_config: MysqlConfig,
+                 table_save:  str,
+                 daemon: bool = True) -> None:
+        super().__init__(name, mysql_config, table_save, daemon)
+
+    def run(self) -> None:
+
+        while self._running:
+            try:
+                _id, url = self.q.get_nowait()
+            except queue.Empty:
+                break
+
+            r = self.session.get(url)
+
+            selector = etree.HTML(r.text)
+
+            description = selector.xpath(
+                './/p[@itemprop="description" and @class="text-section"]/text()'
+            )[0].strip()
+
+            review_count = 0
+
+            review_count_container = selector.xpath(
+                './/meta[@itemprop="reviewCount"]/@content'
+            )
+
+            if review_count_container:
+                review_count = review_count_container[0]
+
+            review_url = None
+
+            review_url_container = selector.xpath(
+                './/span[@class="btn btn-box btn-all-reviews"]/@data-href'
+            )
+
+            if review_url_container:
+                review_url = 'https://www.eyebuydirect.com' + review_url_container[0]
+
+            material = selector.xpath(
+                './/a[@data-event-label="Materials"]/text()'
+            )[0]
+
+            self.update(f'id = {_id}', {
+                'description': description,
+                'review_count': review_count,
+                'review_url': review_url,
+                'material': material
+            })
+
+
+def main() -> None:
+    spider.run_spider(EbdProductListSpider, MYSQL_TABLE_SAVE_EBD, 1, MYSQL_CONFIG)
+
+    mysql_sql = 'SELECT id, url FROM {}'. \
+        format(MYSQL_TABLE_SAVE_EBD)
+    EbdProductDetailSpider.create_task_list(MYSQL_CONFIG, mysql_sql)
+    spider.run_spider(
+        EbdProductDetailSpider,
+        MYSQL_TABLE_SAVE_EBD,
+        THREAD_NUM,
+        MYSQL_CONFIG
+    )
+
+
+if __name__ == '__main__':
+    main()
